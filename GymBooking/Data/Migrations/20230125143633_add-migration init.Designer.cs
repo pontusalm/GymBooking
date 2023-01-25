85bad1120db46256e2e11bc79745a1138621f2b8
pontusalm <pontus.alm@gmail.com>
2023-01-25 18:30:09 +0100

    Build ok

--- GymBooking/Data/Migrations/20230125143633_add-migration init.Designer.cs
+++ GymBooking/Data/Migrations/20230125143633_add-migration init.Designer.cs
@@ -4,6 +4,7 @@ using GymBooking.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GymBooking.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230125143633_add-migration init")]
+    partial class addmigrationinit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
